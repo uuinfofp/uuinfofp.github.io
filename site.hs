d87a3cd165b82c92cd574429901d6a93865880c1
Frank Staals <frank@fstaals.net>
2024-08-29 10:50:23 +0200

    updated exercise pages
    - put the solutions on a separate page
    - highlighting which exercises to do in class and which to do at home

--- site.hs
+++ site.hs
@@ -2,6 +2,8 @@
 {-# LANGUAGE OverloadedStrings #-}
 import Hakyll
 import Text.Pandoc.Options
+import Text.Pandoc.Definition
+import Text.Pandoc.Walk
 
 --------------------------------------------------------------------------------
 main :: IO ()
@@ -45,11 +47,24 @@ main = hakyllWith myConfiguration $ do
             >>= loadAndApplyTemplate "templates/default.html" defaultContext
             >>= relativizeUrls
 
-    match ("*.org" .||. "exercises/*.org") $ do
+    match "*.org" $ do
         route   $ setExtension "html"
         compile $ pandocCompiler'
             >>= loadAndApplyTemplate "templates/default.html" defaultContext
             >>= relativizeUrls
+
+    match "exercises/*.org" $ do
+        route   $ setExtension "html"
+        compile $ exerciseCompiler NoSolutions
+            >>= loadAndApplyTemplate "templates/default.html" defaultContext
+            >>= relativizeUrls
+
+    match "exercises/*.org" $ version "solutions" $ do
+        route   $ setExtension ".solutions.html"
+        compile $ exerciseCompiler WithSolutions
+            >>= loadAndApplyTemplate "templates/default.html" defaultContext
+            >>= relativizeUrls
+
 
 
     match "templates/*" $
@@ -60,16 +75,45 @@ copyAsIs = do
     compile copyFileCompiler
 
 
-pandocCompiler' = pandocCompilerWith defaultHakyllReaderOptions writerOptions
-  where
-    -- use mathjax
-    writerOptions = defaultHakyllWriterOptions {
-                      writerHTMLMathMethod = MathJax defaultMathJaxURL
+-- use mathjax
+defaultWriterOptons = defaultHakyllWriterOptions {
+                        writerHTMLMathMethod = MathJax defaultMathJaxURL
                       }
 
+data WithSolutions = NoSolutions | WithSolutions deriving (Show,Eq)
+
+
+exerciseCompiler sols =
+    pandocCompilerWithTransform defaultHakyllReaderOptions writerOptions transform
+  where
+    writerOptions = defaultWriterOptons { writerNumberSections = True }
+    transform = case sols of
+      WithSolutions -> id
+      NoSolutions   -> noSolutions
+
+
+-- | Our default pandoc compiler
+pandocCompiler' = pandocCompilerWith defaultHakyllReaderOptions defaultWriterOptons
 
 
 myConfiguration :: Configuration
 myConfiguration = defaultConfiguration {
     deployCommand = "rsync -avh _site/* gemini.science.uu.nl:/science/wwwprojects/ics/www/docs/vakken/fp/2024"
   }
+
+
+
+
+--------------------------------------------------------------------------------
+
+noSolutions :: Pandoc -> Pandoc
+noSolutions = walk (filter noSolutions')
+  where
+    noSolutions'   :: Block -> Bool
+    noSolutions' b = case b of
+      CodeBlock attr _ -> not $ hasAttribute "solution" attr
+      _                -> True
+
+    hasAttribute needle (_id, _classes, kvs) = case lookup needle kvs of
+                                                 Just "yes" -> True
+                                                 _          -> False
